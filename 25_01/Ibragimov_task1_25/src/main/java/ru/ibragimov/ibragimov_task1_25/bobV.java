a704f440d43b9af0f1735ff1b2a27d41611f6f34
IbragimovTimur235 <timuribragimov8307@gmail.com>
2025-01-27 12:07:58 +0300

    Add files via upload

---
+++ 25_01/Ibragimov_task1_25/src/main/java/ru/ibragimov/ibragimov_task1_25/bobV.java
@@ -0,0 +1,79 @@
+package ru.ibragimov.ibragimov_task1_25;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+
+import java.util.List;
+
+public class bobV {
+
+    @FXML
+    private Label Max;
+
+    @FXML
+    private Button btn1;
+
+    @FXML
+    private Button btn2;
+
+    @FXML
+    private Button btnO;
+
+    @FXML
+    private Label lb;
+
+    @FXML
+    private ListView<Integer> uchat;
+
+    @FXML
+    private TextField vvoduch;
+
+    @FXML
+    private Label yn;
+
+    @FXML
+    void bat1(ActionEvent event) {
+        if (vvoduch.getText().isEmpty()) {
+            return;
+        }
+        try {
+            int u = Integer.parseInt((vvoduch.getText().toString()));
+            uchat.getItems().add(u);
+        } catch (NumberFormatException ex) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("введены некоректные денные");
+            alert.showAndWait();
+        } finally {
+            vvoduch.setText("");
+        }
+    }
+
+    @FXML
+    void bat2(ActionEvent event) {
+        List<Integer> data = uchat.getItems();
+        int n= data.get(0);
+        int max = 0;
+        String s = "No";
+        for (int i = 1; i < n; i++) {
+            int m = data.get(i);
+            if (m > max)
+                max = m;
+            if (m == 0)
+                s = "Yes";
+
+
+        }
+        Max.setText(String.valueOf(max));
+        yn.setText(s);
+    }
+
+    @FXML
+    void batO(ActionEvent event) {
+        Platform.exit();
+
+    }
+
+}
