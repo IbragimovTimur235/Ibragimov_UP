f17de02edd819b3c808c09247612550b76c4ae4b
IbragimovTimur235 <timuribragimov8307@gmail.com>
2025-01-28 16:11:30 +0300

    Add files via upload

---
+++ Ibragimov_task1_28-9/src/main/java/ru/ibragimov/ibragimov_task1_289/controller/list1.java
@@ -0,0 +1,17 @@
+package ru.ibragimov.ibragimov_task1_289.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class list1 {
+
+    @FXML
+    private Button btn;
+
+    @FXML
+    void but(ActionEvent event) {
+
+    }
+
+}
