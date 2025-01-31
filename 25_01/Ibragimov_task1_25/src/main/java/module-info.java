528b2d930a86773b2277fc7be38114f369d032e5
IbragimovTimur235 <timuribragimov8307@gmail.com>
2025-01-31 11:01:35 +0300

    Delete 25_01 directory

--- 25_01/Ibragimov_task1_25/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ru.ibragimov.ibragimov_task1_25 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens ru.ibragimov.ibragimov_task1_25 to javafx.fxml;
-    exports ru.ibragimov.ibragimov_task1_25;
-}
--- 25_01/Ibragimov_task4_25/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ru.ibragimov.ibragimov_task4_25 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens ru.ibragimov.ibragimov_task4_25 to javafx.fxml;
-    exports ru.ibragimov.ibragimov_task4_25;
-}
